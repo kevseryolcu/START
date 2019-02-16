5cb5fa6ebb346e1b91a68f14917c08232aad49d7
Kevser Yolcu <kyolcu@gtu.edu.tr>
2019-02-17 00:20:21 +0300

    Added start page and images

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 var mainboard = document.getElementById("mainboard");
 var square = document.createElement("div");
+// var ctx= mainboard.getc
 square.className = "square";
-mainboard.appendChild(square);        
+mainboard.appendChild(square);       
+
+square.addEventListener("click", onClickSquare);
+
+function onClickSquare() {
+  console.log("square clicked!");
+  
+}
+
+var squareImg = document.createElement("img");
+squareImg.setAttribute("src", "./img/startLogo.png");
+square.appendChild(squareImg);
+
---
+++ start.js
@@ -0,0 +1,1 @@
+var startLogo = document.getElementById("startLogo");
