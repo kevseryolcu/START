717079db6eb51a8b9fe1b9cc215ec1c0e2eb01db
Kevser Yolcu <kyolcu@gtu.edu.tr>
2019-02-17 03:05:46 +0300

    Added win

--- script.js
+++ script.js
@@ -4,14 +4,36 @@ var square = document.createElement("div");
 square.className = "square";
 mainboard.appendChild(square);       
 
+
+var squareImgs = ["./img/triangle.png", "./img/triangle90.png", "./img/triangle180.png", "./img/triangle270.png"]
+var squareImg = document.createElement("img");
+squareImg.setAttribute("id", "squareImgs");
+squareImg.setAttribute("src", "./img/triangle.png");
+square.appendChild(squareImg);
+square.style.animation = "circle 10s linear infinite";
+
 square.addEventListener("click", onClickSquare);
 
+var squareIndexActual = 3;
+var squareIndex = 0;
 function onClickSquare() {
   console.log("square clicked!");
-  
+  if(squareIndex == 3) {
+      squareIndex = 0;
+  }
+  else {
+      squareIndex++;
+  }
+    squareImg.setAttribute("src", squareImgs[squareIndex]);
 }
 
-var squareImg = document.createElement("img");
-squareImg.setAttribute("src", "./img/startLogo.png");
-square.appendChild(squareImg);
 
+setInterval(function(){
+    if(squareIndex === squareIndexActual){
+        console.log("You win!");
+        square.style.animation = "";
+    }
+    else {
+        console.log("loser");
+    }
+},10000)
