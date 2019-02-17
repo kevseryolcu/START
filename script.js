7f666f3b41e9ea5169953f1f44d8c22da9013ce0
Kevser Yolcu <kyolcu@gtu.edu.tr>
2019-02-17 04:53:57 +0300

    second object

--- script.js
+++ script.js
@@ -1,39 +1,86 @@
 var mainboard = document.getElementById("mainboard");
-var square = document.createElement("div");
-// var ctx= mainboard.getc
-square.className = "square";
-mainboard.appendChild(square);       
+//  triangle
+var triangle = document.createElement("div");
+triangle.className = "triangle";
+mainboard.appendChild(triangle);       
 
+var triangleImgs = ["./img/triangle.png", "./img/triangle90.png", "./img/triangle180.png", "./img/triangle270.png"]
+var triangleImg = document.createElement("img");
+triangleImg.setAttribute("id", "triangleImgs");
+triangle.appendChild(triangleImg);
+triangle.style.marginLeft = '500px';
+// triangle.style.animation = "triangle 5s linear infinite";
 
-var squareImgs = ["./img/triangle.png", "./img/triangle90.png", "./img/triangle180.png", "./img/triangle270.png"]
-var squareImg = document.createElement("img");
-squareImg.setAttribute("id", "squareImgs");
-squareImg.setAttribute("src", "./img/triangle.png");
-square.appendChild(squareImg);
-square.style.animation = "circle 10s linear infinite";
+triangle.addEventListener("click", onClicktriangle);
 
-square.addEventListener("click", onClickSquare);
-
-var squareIndexActual = 3;
-var squareIndex = 0;
-function onClickSquare() {
-  console.log("square clicked!");
-  if(squareIndex == 3) {
-      squareIndex = 0;
+var triangleIndexActual = Math.floor(Math.random() * 4);
+var triangleIndex = Math.floor(Math.random() * 4);
+triangleImg.setAttribute("src", triangleImgs[triangleIndexActual]);
+function onClicktriangle() {
+  console.log("triangle clicked!");
+  if(triangleIndex == 3) {
+      triangleIndex = 0;
   }
   else {
-      squareIndex++;
+      triangleIndex++;
   }
-    squareImg.setAttribute("src", squareImgs[squareIndex]);
+    triangleImg.setAttribute("src", triangleImgs[triangleIndex]);
 }
 
+// circle
+var circle = document.createElement("div");
+circle.className = "circle";
+mainboard.appendChild(circle);       
 
+var circleImgs = ["./img/circle13.png", "./img/circle2.png"]
+var circleImg = document.createElement("img");
+circleImg.setAttribute("id", "circleImgs");
+circle.appendChild(circleImg);
+// circle.style.animation = "circle 5s linear infinite";
+circle.style.marginLeft = '500px';
+circle.addEventListener("click", onClickcircle);
+window.addEventListener("load", onLoadCircle);
+
+var circleIndexActual = Math.floor(Math.random() * 2);
+var circleIndex = circleIndexActual ? 0 : 1;
+circleImg.setAttribute("src", circleImgs[circleIndexActual]);
+function onClickcircle() {
+  console.log("circle clicked!");
+  if(circleIndex == 1) {
+      circleIndex = 0;
+  }
+  else {
+      circleIndex++;
+  }
+    circleImg.setAttribute("src", circleImgs[circleIndex]);
+}
+
+function onLoadCircle() {
+    setTimeout(function(){ 
+        animation();
+    },5000);
+}
+
+function animation() {
+    triangle.style.animation = "triangle 5s linear infinite";
+    circle.style.animation = "circle 5s linear infinite";
+}
+
+// common
 setInterval(function(){
-    if(squareIndex === squareIndexActual){
-        console.log("You win!");
-        square.style.animation = "";
+    if(triangleIndex === triangleIndexActual){
+        triangle.style.animation = "";
     }
+    if(circleIndex === circleIndexActual){
+        circle.style.animation = "";
+    }
+
+    if(triangleIndex === triangleIndexActual && circleIndex === circleIndexActual) {      
+        console.log("You win!");
+        //next chapter button
+    }
+    
     else {
         console.log("loser");
     }
-},10000)
+},5000)
